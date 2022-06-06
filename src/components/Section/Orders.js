1877e44e64ec6d976ee24f943833e972f23d703a
Sidhar Araujo <47336010+sidhara@users.noreply.github.com>
2022-06-06 16:36:26 -0500

    input quantity validations

--- src/components/Section/Orders.js
+++ src/components/Section/Orders.js
@@ -5,6 +5,29 @@ export const Orders=()=>{
 
     const [products, setProducts]=useState(data);
 
+    React.useEffect(()=>{//this is after rendering the component
+        var input=document.getElementById('input')
+        var selectedProductStock=products[0].In_Stock
+
+        input.setAttribute("max", selectedProductStock);
+        var combobox=document.getElementById('combobox');
+        combobox.addEventListener('click',(event)=>{//controlling the max amout in the quantity input
+            var selectedId=combobox.value;
+            products.map((product)=>{
+                if(product.Id==selectedId){
+                    selectedProductStock=product.In_Stock;
+                    input.setAttribute("max", selectedProductStock);
+                }
+            })
+        })
+
+        input.addEventListener('keyup',(event)=>{//controlling the max amout in the quantity input
+            if(input.value>selectedProductStock){
+                input.value=input.value.slice(0,input.value.length-1)
+            }
+        })
+    }, [])
+
     return <div>
         <h1 className="orders_title">Orders</h1>
         <div className="orders_container">
